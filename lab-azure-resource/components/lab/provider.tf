da689d84d66be54c048e84301dc11804ebe63494
Felix Eyetan <felix.eyetan@justice.gov.uk>
2023-06-12 20:31:03 +0100

    content update for setting up repo (#35)
    * content update for setting up repo
    
    ---------
    
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>
    Co-authored-by: Jordan Hoey <jordan.hoey@justice.gov.uk>

---
+++ lab-azure-resource/components/lab/provider.tf
@@ -0,0 +1,30 @@
+terraform {
+  required_version = ">= 1.5.0"
+  backend "azurerm" {} // When using remote
+  #backend "local" {} // When using local
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "azurerm" {
+  subscription_id            = var.subscription_id
+  skip_provider_registration = "true"
+  features {}
+  alias = "labs"
+}
+
+provider "azurerm" {
+  subscription_id            = var.hub_sbox_subscription_id
+  skip_provider_registration = "true"
+  features {}
+  alias = "hub-sbox"
+}
+
+provider "azurerm" {
+  subscription_id            = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
+  skip_provider_registration = "true"
+  features {}
+  alias = "vpn"
+}
