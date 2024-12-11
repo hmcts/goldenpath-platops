c543d6b37ffd55f470ba8c1bf3bb30cade29cb30
dawidstrozak <123395719+dawidstrozak@users.noreply.github.com>
2024-12-11 15:12:13 +0000

    Replace deprecated skip_provider_registration property (#56)
    * Replace 'skip_provider_registration' property (as this is deprecated now) with equivalent 'resource_provider_registrations' property
    * Fix the link to the sandbox polcy rules file in the virtual networks section

--- lab-azure-resource/components/lab/provider.tf
+++ lab-azure-resource/components/lab/provider.tf
@@ -9,22 +9,22 @@ provider "azurerm" {
 }
 
 provider "azurerm" {
-  subscription_id            = var.subscription_id
-  skip_provider_registration = "true"
+  subscription_id                 = var.subscription_id
+  resource_provider_registrations = "none"
   features {}
   alias = "labs"
 }
 
 provider "azurerm" {
-  subscription_id            = var.hub_sbox_subscription_id
-  skip_provider_registration = "true"
+  subscription_id                 = var.hub_sbox_subscription_id
+  resource_provider_registrations = "none"
   features {}
   alias = "hub-sbox"
 }
 
 provider "azurerm" {
-  subscription_id            = "ed302caf-ec27-4c64-a05e-85731c3ce90e" # Update current VPN subscription id if this no longer exists
-  skip_provider_registration = "true"
+  subscription_id                 = "ed302caf-ec27-4c64-a05e-85731c3ce90e" # Update current VPN subscription id if this no longer exists
+  resource_provider_registrations = "none"
   features {}
   alias = "vpn"
 }
